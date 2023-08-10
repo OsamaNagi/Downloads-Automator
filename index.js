1e22d3e4015e75b744bf1f66d706a7654d12eac8
OsamaNagi <alshahidyosama@gmail.com>
2023-08-10 23:51:38 +0200

    fixed file duplication issue

--- index.js
+++ index.js
@@ -6,89 +6,51 @@ const extensions = require('./extensions');
 const Directory = '/Users/nagi/Downloads';
 const imageDirectory = '/Users/nagi/Downloads/Images';
 const videoDirectory = '/Users/nagi/Downloads/Videos';
-const musicDirectory = '/Users/nagi/Downloads/Music';
-const SfxDirectory = '/Users/nagi/Downloads/SFX';
-const documentDirectory = '/Users/nagi/Downloads/Documents';
+const audioDirectory = '/Users/nagi/Downloads/Audios';
 
-function makeUnique(dest, name) {
-	const { name: filename, ext: extension } = path.parse(name);
-	let counter = 1;
+function makeUnique (dir, file) {
+    const { name, ext } = path.parse(file);
+    let counter = 1;
 
-	while (fs.existsSync(path.join(dest, name))) {
-		name = `${filename}-${counter}${extension}`;
-		counter++;
-	}
+    while (fs.existsSync(path.join(dir, file))) {
+        file = `${ name }(${ counter })${ ext }`;
+        counter++;
+    }
 
-	return name;
+    return file;
 }
 
-function moveFile(dest, entry, name) {
-	if (fs.existsSync(path.join(dest, name))) {
-		const uniqueName = makeUnique(dest, name);
-		const oldName = path.join(dest, name);
-		const newName = path.join(dest, uniqueName);
-		escapeSpecialCharacters(newName);
-		fs.renameSync(oldName, newName);
-	}
+const moveFile = (sourcePath, destinationDir) => {
+    const fileName = path.basename(sourcePath);
+    const destinationPath = path.join(destinationDir, fileName);
 
-	fs.renameSync(entry, path.join(dest, name));
-}
+    if (fs.existsSync(destinationPath)) {
+        const uniqueFileName = makeUnique(destinationDir, fileName);
+        const newPath = path.join(destinationDir, uniqueFileName);
+        fs.renameSync(sourcePath, newPath);
+        console.log(`File moved with unique name: ${ newPath }`);
+    } else {
+        fs.renameSync(sourcePath, destinationPath);
+        console.log(`File moved: ${ destinationPath }`);
+    }
+};
 
 const watcher = chokidar.watch(Directory, { persistent: true });
 
-watcher.on('add', (entry) => {
-	const name = path.basename(entry);
+watcher.on('change', (file) => {
+    console.log('File', file, 'has been added');
 
-	checkAudioFiles(entry, name);
-	checkVideoFiles(entry, name);
-	checkImageFiles(entry, name);
-	checkDocumentFiles(entry, name);
+    const extension = path.extname(file).toLowerCase();
+
+    if (extensions.imageExtensions.includes(extension)) {
+        moveFile(file, imageDirectory);
+    } else if (extensions.videoExtensions.includes(extension)) {
+        moveFile(file, videoDirectory);
+    } else if (extensions.audioExtensions.includes(extension)) {
+        moveFile(file, audioDirectory);
+    } else if (extensions.documentExtensions.includes(extension)) {
+        moveFile(file, documentDirectory);
+    } else {
+        console.log('Unknown file type, not moving:', file);
+    }
 });
-
-function checkAudioFiles(entry, name) {
-	extensions.audioExtensions.forEach((audioExtension) => {
-		if (name.toLowerCase().endsWith(audioExtension)) {
-			const dest = fs.statSync(entry).size < 10000000 || name.includes('SFX') ? SfxDirectory : musicDirectory;
-			moveFile(dest, entry, name);
-			console.log(`Moved audio file: ${name}`);
-		}
-	});
-}
-
-function checkVideoFiles(entry, name) {
-	extensions.videoExtensions.forEach((videoExtension) => {
-		if (name.toLowerCase().endsWith(videoExtension)) {
-			moveFile(videoDirectory, entry, name);
-			console.log(`Moved video file: ${name}`);
-		}
-	});
-}
-
-function checkImageFiles(entry, name) {
-	extensions.imageExtensions.forEach((imageExtension) => {
-		if (name.toLowerCase().endsWith(imageExtension)) {
-			moveFile(imageDirectory, entry, name);
-			console.log(`Moved image file: ${name}`);
-		}
-	});
-}
-
-function checkDocumentFiles(entry, name) {
-	extensions.documentExtensions.forEach((documentExtension) => {
-		if (name.toLowerCase().endsWith(documentExtension)) {
-			moveFile(documentDirectory, entry, name);
-			console.log(`Moved document file: ${name}`);
-		}
-	});
-}
-
-function escapeSpecialCharacters(fileName) {
-	const specialCharacters = /[-\/\\^$*+?.()|[\]{}]/g;
-
-	if (specialCharacters.test(fileName)) {
-		const escapedFileName = fileName.replace(specialCharacters, '\\$&');
-		return escapedFileName;
-	}
-
-	return fileName;
-}
